f23cda1717817dd74122884652f29e60c0a48fb6
yungsabirka <sabirsayadov18@gmail.com>
2023-12-23 16:24:23 +0200

    Fix up

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.header_link').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
